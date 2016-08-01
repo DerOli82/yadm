6e4de5682c89221f0f667fd461993bfd1068a846
DerOli82 <deroli82@alaoli.de>
2016-08-01 09:56:02 +0200

    Sync dimensions on client connection

--- src/main/java/de/alaoli/games/minecraft/mods/yadm/event/DimensionEvent.java
+++ src/main/java/de/alaoli/games/minecraft/mods/yadm/event/DimensionEvent.java
@@ -1,11 +1,38 @@
 package de.alaoli.games.minecraft.mods.yadm.event;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map.Entry;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
 import de.alaoli.games.minecraft.mods.yadm.YADM;
+import de.alaoli.games.minecraft.mods.yadm.data.DataObject;
+import de.alaoli.games.minecraft.mods.yadm.data.Dimension;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraftforge.event.world.WorldEvent;
 
 public class DimensionEvent 
 {
+    @SubscribeEvent
+    public void onClientConnected( FMLNetworkEvent.ServerConnectionFromClientEvent event )
+    {
+    	//Sync Dimensions
+    	if( !YADM.proxy.getDimensionManager().isEmpty() )
+    	{
+	    	EntityPlayerMP player = ((NetHandlerPlayServer) event.handler).playerEntity;
+	
+	    	Set<Dimension> dimensions = new HashSet<Dimension>(); 
+	    			
+	    	for( Entry<String, DataObject> entry : YADM.proxy.getDimensionManager().getAll() )
+	    	{
+	    		dimensions.add( (Dimension)entry );
+	    	}	
+	    	YADM.proxy.syncDimension(dimensions, player);
+    	}
+    }
+    
 	@SubscribeEvent
 	public void onWorldSave( WorldEvent.Save event )
 	{
