5fd83e2d414f70a93141e9e8d82d95d4a0f63b22
DerOli82 <deroli82@alaoli.de>
2016-08-07 16:03:46 +0200

    inital commit
    WorldBuilder creates custom WorldProvider

---
+++ src/main/java/de/alaoli/games/minecraft/mods/yadm/world/WorldBuilder.java
@@ -0,0 +1,185 @@
+package de.alaoli.games.minecraft.mods.yadm.world;
+
+import java.lang.reflect.Field;
+import java.util.Hashtable;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import de.alaoli.games.minecraft.mods.yadm.Log;
+import de.alaoli.games.minecraft.mods.yadm.data.Dimension;
+import de.alaoli.games.minecraft.mods.yadm.data.DimensionTemplate;
+import de.alaoli.games.minecraft.mods.yadm.interceptor.worldprovider.DimensionFieldAccessor;
+import de.alaoli.games.minecraft.mods.yadm.interceptor.worldprovider.GetDimensionNameInterceptor;
+import de.alaoli.games.minecraft.mods.yadm.interceptor.worldprovider.GetSeedInterceptor;
+import de.alaoli.games.minecraft.mods.yadm.interceptor.worldprovider.RegisterWorldChunkManagerPostInterceptor;
+import de.alaoli.games.minecraft.mods.yadm.interceptor.worldprovider.RegisterWorldChunkManagerPreInterceptor;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.NamingStrategy;
+import net.bytebuddy.description.modifier.Visibility;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.FieldAccessor;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.SuperMethodCall;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.minecraft.world.WorldProvider;
+import net.minecraft.world.WorldType;
+import net.minecraftforge.common.DimensionManager;
+
+public class WorldBuilder 
+{
+	/********************************************************************************
+	 * Attributes
+	 ********************************************************************************/
+		
+	public static final WorldBuilder instance = new WorldBuilder();
+	
+	private Hashtable<Integer, Class<? extends WorldProvider>> worldProviders;
+
+	private Hashtable<String, WorldType> worldTypes;	
+	
+	private WorldBuilder()
+	{
+		this.initProvider();
+		this.initTypes();
+	}
+	
+	/********************************************************************************
+	 * Methods
+	 ********************************************************************************/
+	
+	@SuppressWarnings( "unchecked" )
+	private void initProvider()
+	{
+		this.worldProviders = new Hashtable<Integer, Class<? extends WorldProvider>>();
+		
+		try 
+		{
+			Hashtable<Integer, Class<? extends WorldProvider>> providers;
+			Field providersField = DimensionManager.class.getDeclaredField( "providers" );
+			
+			providersField.setAccessible( true );
+			
+			this.worldProviders = (Hashtable<Integer, Class<? extends WorldProvider>>) providersField.get( null );
+		}
+		catch ( NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e )
+		{
+			Log.error( e.getMessage() );
+		}
+		
+	}
+	
+	private void initTypes()
+	{
+		WorldType type;
+		String name;
+		
+		this.worldTypes = new Hashtable<String, WorldType>();
+		
+		for( int i = 0; i < WorldType.worldTypes.length; i++ )
+		{
+			type = WorldType.worldTypes[ i ];
+			
+			if( type != null )
+			{
+				name = type.getWorldTypeName();
+			
+				//Minecraft doesn't allow to create DEFAULT_1_1 ( see net.minecraft.world.WorldType )
+				if ( !name.equals( WorldType.DEFAULT_1_1.getWorldTypeName() ) )
+				{
+					this.worldTypes.put( name, type );
+				}
+			}
+		}
+	}	
+	
+	public boolean existsWorldProvider( int provider )
+	{
+		return this.worldProviders.containsKey( provider );
+	}
+	
+	public boolean existsWorldType( String type )
+	{
+		return this.worldTypes.containsKey( type );
+	}
+	
+	@SuppressWarnings( "unchecked" )
+	public WorldProvider createProvider( Dimension dimension ) throws ClassNotFoundException, InstantiationException, IllegalAccessException  
+	{
+		Class<? extends WorldProvider> providerClass;
+		
+		switch( dimension.getSettings().getProviderName() )
+		{
+			case DimensionTemplate.PROVIDER_NETHER :
+				providerClass = this.worldProviders.get( -1 );
+				break;
+				
+			case DimensionTemplate.PROVIDER_OVERWORLD :
+				providerClass = this.worldProviders.get( 0 );
+				break;
+				
+			case DimensionTemplate.PROVIDER_END :
+				providerClass = this.worldProviders.get( 1 );
+				break;				
+				
+			default :
+				//TODO Custom Provider
+				providerClass = this.worldProviders.get( 0 );
+				break;
+		}		
+		String registerWorldChunkManagerMethodName = ReflectionHelper.findMethod( 
+			WorldProvider.class, (WorldProvider)providerClass.newInstance(), 
+			new String[]{ "func_76572_b", "registerWorldChunkManager" } 
+		).getName();
+		String getDimensionNameMethodName = ReflectionHelper.findMethod( 
+			WorldProvider.class, (WorldProvider)providerClass.newInstance(), 
+			new String[]{ "func_80007_l", "getDimensionName" } 
+		).getName();
+		String getSeedMethodName = ReflectionHelper.findMethod( 
+			WorldProvider.class, (WorldProvider)providerClass.newInstance(), 
+			new String[]{ "func_72905_C", "getSeed" } 
+		).getName();	
+		
+		Class<?> dynamicType = new ByteBuddy()
+			.with( new NamingStrategy.SuffixingRandom( "YADM" ) )
+			.subclass( providerClass )
+			.defineField( "dimensionYADM", Dimension.class, Visibility.PROTECTED )
+			.implement( DimensionFieldAccessor.class )
+			.intercept( FieldAccessor.ofBeanProperty() )
+			.method( ElementMatchers.named( registerWorldChunkManagerMethodName ) )
+			.intercept( 
+				MethodDelegation.to( RegisterWorldChunkManagerPreInterceptor.class )
+				.andThen( SuperMethodCall.INSTANCE
+				.andThen( MethodDelegation.to( RegisterWorldChunkManagerPostInterceptor.class ) ) ) 
+			)
+			.method( ElementMatchers.named( getDimensionNameMethodName ) )
+			.intercept( MethodDelegation.to( GetDimensionNameInterceptor.class ) )
+			.method( ElementMatchers.named( getSeedMethodName ) )
+			.intercept( MethodDelegation.to( GetSeedInterceptor.class ) )
+			.make()
+			.load( getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER )
+			.getLoaded();
+		
+		return (WorldProvider) dynamicType.newInstance();
+	}	
+	
+	public int registerProvider( WorldProvider provider, boolean keepLoaded )
+	{
+		int nextId = 0;
+		
+		while( !DimensionManager.registerProviderType( nextId, provider.getClass(), keepLoaded))
+		{
+			nextId++;
+		}
+		
+		return nextId;
+	}
+	
+	/********************************************************************************
+	 * Methods - Getter/Setter
+	 ********************************************************************************/
+	
+	public WorldType getWorldType( String name )
+	{
+		return this.worldTypes.get( name );
+	}
+	
+
+}
