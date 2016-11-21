8bb5821e07950fca5af873fafa06c0dae2f9863d
DerOli82 <deroli82@alaoli.de>
2016-11-21 14:55:19 +0100

    clean up

--- src/main/java/de/alaoli/games/minecraft/mods/yadm/event/handler/DimensionEvent.java
+++ src/main/java/de/alaoli/games/minecraft/mods/yadm/event/handler/DimensionEvent.java
@@ -2,10 +2,7 @@ package de.alaoli.games.minecraft.mods.yadm.event.handler;
 
 import java.io.IOException;
 
-import javax.vecmath.Vector2d;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import de.alaoli.games.minecraft.mods.yadm.Log;
 import de.alaoli.games.minecraft.mods.yadm.data.DataException;
 import de.alaoli.games.minecraft.mods.yadm.data.Dimension;
 import de.alaoli.games.minecraft.mods.yadm.data.settings.SettingType;
@@ -14,7 +11,6 @@ import de.alaoli.games.minecraft.mods.yadm.event.WorldBorderEvent;
 import de.alaoli.games.minecraft.mods.yadm.manager.PlayerManager;
 import de.alaoli.games.minecraft.mods.yadm.manager.YADimensionManager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityEvent.EnteringChunk;
 import net.minecraftforge.event.world.WorldEvent;
 
