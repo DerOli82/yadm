cdf58f945d0ad10542e02fedc94d167468f1eaec
DerOli82 <deroli82@alaoli.de>
2017-02-07 19:39:42 +0100

    Moved to https://github.com/DerOli82/common-library

--- src/main/java/de/alaoli/games/minecraft/mods/lib/common/config/Section.java
+++
@@ -1,8 +0,0 @@
-package de.alaoli.games.minecraft.mods.lib.common.config;
-
-import de.alaoli.games.minecraft.mods.lib.common.json.JsonSerializable;
-
-public interface Section extends JsonSerializable 
-{
-	public String getSectionName();
-}
--- src/main/java/de/alaoli/games/minecraft/mods/lib/common/network/Packageable.java
+++
@@ -1,9 +0,0 @@
-package de.alaoli.games.minecraft.mods.lib.common.network;
-
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface Packageable 
-{
-	public void writeToNBT( NBTTagCompound tagCompound );
-	public void readFromNBT( NBTTagCompound tagCompound );
-}
